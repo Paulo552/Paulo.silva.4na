5695aec53b9350a3364fa579985e9a5ab52857a0
Paulo552 <paulo.vss@hotmail.com>
2025-02-26 00:16:23 +0000

    aula03

---
+++ aula03/desafioaula03.py
@@ -0,0 +1,7 @@
+from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, f1_score, roc_curve, auc
+import matplotlib.pyplot as plt
+import numpy as np
